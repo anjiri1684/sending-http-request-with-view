85ffb53f1acec34cc093c51813264e08057ba24e
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-29 15:51:21 +0300

    "Removed survey-submit event and data storage from App.vue, and added data fetching and loading functionality to UserExperiences.vue"

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
@@ -21,9 +23,36 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExperiences() {
+      fetch(
+        'https://vue-http-demo-37dc2-default-rtdb.firebaseio.com/surveys.json'
+      )
+        .then((response) => {
+          if (response.ok) {
+            response.json();
+          }
+        })
+        .then((data) => {
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+        });
+    },
   },
 };
 </script>
